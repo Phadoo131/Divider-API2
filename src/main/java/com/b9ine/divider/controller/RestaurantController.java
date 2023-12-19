d981f15f739b6214bee0c70c13f4c46cc25ccb95
Narapong Hongthai <narapong.h@g-able.com>
2023-12-19 09:20:36 +0700

    fixing RequestMapping path

--- src/main/java/com/b9ine/divider/controller/RestaurantController.java
+++ src/main/java/com/b9ine/divider/controller/RestaurantController.java
@@ -11,7 +11,7 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("/divider/")
+@RequestMapping("/divider")
 public class RestaurantController {
 
     @Autowired
